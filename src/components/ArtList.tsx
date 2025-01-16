61f0939e86e907513f7f8e979b9af9f9cf2c8547
Shaurya-Sarma <shaurya.sarma@gmail.com>
2025-01-15 23:52:26 -0500

    dropdown filter bar for sm screens

--- src/components/ArtList.tsx
+++ src/components/ArtList.tsx
@@ -1,36 +1,50 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { ART_IMAGES } from "../helpers/constants";
 
 function ArtList() {
-  const [columns, setColumns] = useState(1);
+  const [selectedImage, setSelectedImage] = useState<string>();
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
-  useEffect(() => {
-    updateColumns(); // Update the number of columns based on screen size
-    window.addEventListener("resize", updateColumns);
-    return () => {
-      window.removeEventListener("resize", updateColumns);
-    };
-  }, []);
+  const openModal = (url: string) => {
+    setIsModalOpen(true);
+    setSelectedImage(url);
+  };
 
-  const updateColumns = () => {
-    if (window.innerWidth >= 768) {
-      setColumns(3); // For large screens
-    } else if (window.innerWidth >= 640) {
-      setColumns(2); // For medium screens
-    } else {
-      setColumns(1); // For small screens
-    }
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedImage("");
   };
 
   return (
-    <div className="flex flex-col items-center mt-10 mb-20">
-      <ul className="gallery">
+    <div className="w-full flex flex-col items-center mt-10 mb-20">
+      {/* Gallery */}
+      <ul className="grid grid-cols-1 md:grid-cols-2 gap-5">
         {ART_IMAGES.map((src, index) => (
-          <li key={index} className="overflow-hidden">
-            <img src={src} alt={`Art ${index + 1}`} className="gallery-item" />
+          <li key={index} className="gallery-item">
+            <img
+              src={src}
+              alt={`Art ${index + 1}`}
+              className="h-full object-cover select-none rounded-sm shadow-md cursor-pointer"
+              onClick={() => openModal(src)}
+            />
           </li>
         ))}
       </ul>
+      {/* Image Modal  */}
+      {/* <Modal
+        isOpen={isModalOpen}
+        // onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        // style={customStyles}
+        className={""}
+        contentLabel="Example Modal"
+      >
+        <img
+          src={selectedImage}
+          alt="thumbnail"
+          className="w-10/12 m-auto align-middle"
+        />
+      </Modal> */}
     </div>
   );
 }
--- src/components/ProjectFilterBar.tsx
+++ src/components/ProjectFilterBar.tsx
@@ -2,6 +2,8 @@ import { useNavigate } from "react-router-dom";
 import { useCursorContext } from "../helpers/CursorContext";
 import StarIcon from "./StarIcon";
 import { PROJECT_FILTER_HEADERS } from "../helpers/constants";
+import { useState } from "react";
+import { useMediaQuery } from "react-responsive";
 
 interface ProjectFilterBarProps {
   setSelectedTag: (tag: string | null) => void;
@@ -14,6 +16,8 @@ function ProjectFilterBar({
 }: ProjectFilterBarProps) {
   const navigate = useNavigate();
   const { setCursorHover } = useCursorContext();
+  const [showDropdown, setShowDropdown] = useState<boolean>(false);
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 768px)" });
 
   return (
     <div className="flex flex-row justify-between items-center z-10">
@@ -28,25 +32,64 @@ function ProjectFilterBar({
       >
         <StarIcon color="#000" size={40} />
       </div>
-      <div className="flex flex-row gap-5">
-        {PROJECT_FILTER_HEADERS.map((str, index) => {
-          const isActive = selectedTag === str.toLowerCase();
 
-          return (
-            <h3
-              key={index}
-              className={`text-xl uppercase font-medium cursor-pointer select-none text-black transition-all duration-500 p-2 ${
-                isActive ? "bg-[#F5B700] " : ""
-              }`}
-              onClick={() =>
-                setSelectedTag(isActive ? null : str.toLowerCase())
-              }
-            >
-              {str}
-            </h3>
-          );
-        })}
-      </div>
+      {/* Filter Dropdown for Smaller Screens */}
+      {isTabletOrMobile && (
+        <div className="relative">
+          <span
+            onClick={() => setShowDropdown(!showDropdown)}
+            className="text-xl uppercase font-medium cursor-pointer select-none text-black transition-all duration-500 p-2"
+          >
+            {selectedTag ? selectedTag + " ▾" : "categories ▾"}
+          </span>
+          {showDropdown && (
+            <div className="absolute top-full right-2 min-w-56 bg-white border rounded-md shadow-md mt-2">
+              <div className="grid grid-rows-3 gap-1 p-2">
+                {PROJECT_FILTER_HEADERS.slice(0, 3).map((str, index) => {
+                  const isActive = selectedTag === str.toLowerCase();
+                  return (
+                    <span
+                      key={index}
+                      className={`uppercase font-medium cursor-pointer select-none text-black transition-all duration-500 p-2 rounded ${
+                        isActive ? "bg-[#F5B700]" : "hover:bg-gray-200"
+                      }`}
+                      onClick={() => {
+                        setSelectedTag(isActive ? null : str.toLowerCase());
+                        setShowDropdown(false);
+                      }}
+                    >
+                      {str}
+                    </span>
+                  );
+                })}
+              </div>
+            </div>
+          )}
+        </div>
+      )}
+
+      {/* Filter Button Bar for Larger Screens */}
+      {!isTabletOrMobile && (
+        <div className="flex flex-row gap-2 md:gap-5">
+          {PROJECT_FILTER_HEADERS.map((str, index) => {
+            const isActive = selectedTag === str.toLowerCase();
+
+            return (
+              <h3
+                key={index}
+                className={`text-base md:text-xl uppercase font-medium cursor-pointer select-none text-black transition-all duration-500 p-2 ${
+                  isActive ? "bg-[#F5B700]" : ""
+                }`}
+                onClick={() =>
+                  setSelectedTag(isActive ? null : str.toLowerCase())
+                }
+              >
+                {str}
+              </h3>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
